e8e3463e18c0b140708cb8f3c0a94f53030bd5ae
moon <dearbmk@hotmail.com>
2022-11-01 18:35:42 +0100

    add loginValidator

---
+++ src/main/java/com/example/projectnote/config/LoginValidator.java
@@ -0,0 +1,34 @@
+package com.example.projectnote.config;
+
+import com.example.projectnote.data.dao.UserDAO;
+import com.example.projectnote.data.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginValidator implements UserDetailsService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity user = userDAO.getUser(username);
+        if (user == null){
+            return null;
+        }
+
+        String pwd = user.getPassword();
+
+        return User.builder()
+                .username(username)
+                .password(pwd)
+                .build();
+    }
+}
+
