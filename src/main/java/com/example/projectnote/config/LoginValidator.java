7f96396c7c8529caaaa968586422db9e83f2d45d
moon <dearbmk@hotmail.com>
2022-11-06 18:37:53 +0100

    return encrypted password

--- src/main/java/com/example/projectnote/config/LoginValidator.java
+++ src/main/java/com/example/projectnote/config/LoginValidator.java
@@ -3,7 +3,6 @@ package com.example.projectnote.config;
 import com.example.projectnote.data.dao.UserDAO;
 import com.example.projectnote.data.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,25 +16,29 @@ public class LoginValidator implements UserDetailsService {
     @Autowired
     private UserDAO userDAO;
 
-    @Bean
-    public BCryptPasswordEncoder encoder(){
-        return new BCryptPasswordEncoder();
-    }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         UserEntity user = userDAO.getUser(username);
-        if (user == null){
-            return null;
+
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User doesnt exist.");
         }
 
         String pwd = user.getPassword();
+        String roles = user.getRoles();
 
         return User.builder()
                 .username(username)
-                .password(pwd)
+                .password(encoder.encode(pwd))
+                .roles(roles)
                 .build();
+
     }
 }
 
