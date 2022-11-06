413402e412e7149553afed86c8e84da3133d6941
moon <dearbmk@hotmail.com>
2022-11-06 18:33:00 +0100

    add init sql data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `USER` (USERNAME, PASSWORD, EMAIL, ROLES) VALUES ('username', 'password','peter@email.com', 'admin');
+INSERT INTO `USER` (USERNAME, PASSWORD, EMAIL, ROLES) VALUES ('Mike', 'passwords','mike@email.com', 'user');
